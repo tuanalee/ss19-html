03a9c0248b36a0b8a3408724f5f8437704e6e5fa
tuanalee <130274706+tuanalee@users.noreply.github.com>
2025-03-03 21:10:36 +0700

    Update bt3.js

--- bt3.js
+++ bt3.js
@@ -1,14 +1,14 @@
 let currency=prompt("Mời bạn nhập tiền tệ cần chuyển đổi(VND or USD)");
 let money=prompt("Mời bạn nhập số tiền cần chuyển đổi")
-let number;
+let numbers;
 switch(currency){
     case "VND":
-        number=money*23000;
-        console.log("USD -> VND: "+number);
+        numbers=money*23000;
+        console.log("USD -> VND: "+numbers);
         break;
     case "USD":
-        number=money/23000;
-        console.log("VND -> USD: "+number);
+        numbers=money/23000;
+        console.log("VND -> USD: "+numbers);
         break;
     default:
         console.log("Yêu cầu bạn nhập không hợp lệ");
