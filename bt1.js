fdf6a24fa3b3442346a75c5974270379f8253940
tuanalee <130274706+tuanalee@users.noreply.github.com>
2025-03-03 21:07:50 +0700

    Update bt1.js

--- bt1.js
+++ bt1.js
@@ -1,11 +1,11 @@
-let month=+prompt("Mời bạn nhập số tháng");
-if(month > 12 && month < 1){
-    if(month == 2){
+let thang=+prompt("Mời bạn nhập số tháng");
+if(thang > 12 && thang < 1){
+    if(thang == 2){
         console.log("Tháng 2 có 28 hoặc 29 ngày năm nhuận");
-    }else if(month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12){
-        console.log("Tháng " + month + "có 31 ngày");
+    }else if(thang == 1 || thang == 3 || thang == 5 || thang == 7 || thang == 8 || thang == 10 || thang == 12){
+        console.log("Tháng " + thang + "có 31 ngày");
     }else{
-        console.log("Tháng " + month + "có 30 ngày");
+        console.log("Tháng " + thang + "có 30 ngày");
     }
 }else{
     console.log("Tháng không hợp lệ");
